1f31ab9bb37552bb936fb96623ae7e98a813d6b5
Dan Baker <dbaker@Daniels-MacBook-Pro-2.local>
2016-12-03 16:16:56 -0500

    ssh

--- index.js
+++ index.js
@@ -10,12 +10,29 @@ catch (err) {
 
 require('./db/db');
 
+var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 var port = process.env.PORT || 3000;
+var  fs = require('fs');
+
+var path = '/etc/letsencrypt/live/www.windowdotlocation.club/';
 
 mainDb.sync().then(function () {
+  try {
+      var options = {
+        key: fs.readFileSync(`${path}privkey.pem`),
+        cert: fs.readFileSync(`${path}cert.pem`),
+      };
+      require('https').createServer(options,app).listen(443, function(){
+        console.log("Express server listening on port " + 443);
+      });
+  } catch (er) {
+      
+  }
   server.listen(port, function () {
     console.log('Server listening at port %d', port);
   });
